5c8c04a9892a641b53a10b7b295e43da7a0a9cd8
kimberley <kimberleyyao@eslite.com>
2021-03-11 11:58:49 +0800

    Implement simple search feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,11 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+  movies = Movie.create(
+    [
+      { name: 'Frozen', description: 'When the newly crowned Queen Elsa accidentally uses her power to turn things into ice o curse her home in infinite winter, her sister, Anna, teams up with a mountain man, his playful reindeer, and a snowman to change the weather condition.' },
+      { name: 'Big Hero 6', description: 'The special bond that develops between plus-sized inflatable robot Baymax, and prodigy Hiro Hemada, who team up with a group of friends to form a band of high-tech heroes.' },
+      { name: 'Blackhat', description: 'A fuloughed convict and his American and Chinese partners hunt a high-level cybercrime network from Chicago to Los Angeles to Hong Kong to jakara.' }
+    ]
+  )
 #   Character.create(name: 'Luke', movie: movies.first)
